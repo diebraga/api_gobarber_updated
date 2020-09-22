f18203f7d7d8ee0325f0e5b8cff8cef283a058ce
unknown <diebraga0055@gmail.com>
2020-09-22 09:09:31 +0100

    formated hour added can't book in the same hour

--- src/routes/appointment.routes.ts
+++ src/routes/appointment.routes.ts
@@ -1,20 +1,38 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
-
+import { startOfHour, parseISO, isEqual } from 'date-fns'
 const appointmentRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
+  const formatedDate = startOfHour(parseISO(date));
+
+  const findAppointmentSameDate = appointments.find(appointment =>
+    isEqual(formatedDate, appointment.date),
+    );
+
+  if (findAppointmentSameDate) {
+    return res
+      .status(400)
+      .json({ message: 'Hour already filled'});
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: formatedDate,
   }
   appointments.push(appointment);
-  
+
   return res.json(appointment);
 })
 

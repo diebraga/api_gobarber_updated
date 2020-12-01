962ece3d997d7b3ae5fcf842fa17bf47c712e16d
unknown <diebraga0055@gmail.com>
2020-12-01 13:16:32 +0000

    isolated button and inpit + props

--- mobile/src/pages/Signin/index.tsx
+++ mobile/src/pages/Signin/index.tsx
@@ -4,11 +4,19 @@ import logo from '../../assets/logo.png';
 
 import { Container, Title } from './styles';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 const Signin: React.FC = () => {
   return(
     <Container>
       <Image source={logo} />
       <Title>Login into your account</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Password" />
+
+      <Button onPress={() => {}}>Login</Button>
     </Container>
   );
 }
